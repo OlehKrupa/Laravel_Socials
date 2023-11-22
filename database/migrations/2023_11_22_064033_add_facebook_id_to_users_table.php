a4f3fd8b9355f0166c89a6c934bc577247055256
Oleh <krupao980@gmail.com>
2023-11-22 19:11:18 +0200

    Twitter oAuth

--- database/migrations/2023_11_22_064033_add_facebook_id_to_users_table.php
+++ database/migrations/2023_11_22_064033_add_facebook_id_to_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('facebook_id')->nullable();
+            $table->dropColumn('facebook_id');
         });
     }
 };
--- resources/views/auth/socialRefs.blade.php
+++ resources/views/auth/socialRefs.blade.php
@@ -5,9 +5,9 @@
     <a href="auth/github/redirect" class="btn btn-secondary btn-block btn-icon mt-2">
         <img src="https://img.icons8.com/color/2x/github.png" alt="GitHub" width="48" height="48">
     </a>
-{{--    <a href="auth/instagram/redirect" class="btn btn-info btn-block btn-icon mt-2">--}}
-{{--        <img src="https://img.icons8.com/color/2x/instagram.png" alt="Instagram" width="48" height="48">--}}
-{{--    </a>--}}
+    <a href="auth/twitter/redirect" class="btn btn-info btn-block btn-icon mt-2">
+        <img src="https://img.icons8.com/color/2x/twitter.png" alt="Twitter" width="48" height="48">
+    </a>
     <a href="auth/facebook/redirect" class="btn btn-info btn-block btn-icon mt-2">
         <img src="https://img.icons8.com/color/2x/facebook.png" alt="Facebook" width="48" height="48">
     </a>
