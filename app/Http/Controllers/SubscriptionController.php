b00246390b5a858b6013eb5762caaa5bcc21b467
Oleh <krupao980@gmail.com>
2023-11-27 10:51:10 +0200

    Formatting

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -10,12 +10,12 @@ class SubscriptionController extends Controller
     public function continueSubscription(User $user)
     {
         $user->update(['is_subscribed' => true]);
-        return redirect()->back();
+//        return redirect()->back();
     }
 
     public function cancelSubscription(User $user)
     {
         $user->update(['is_subscribed' => false]);
-        return redirect()->back();
+//        return redirect()->back();
     }
 }
