fa8ddd19c29f21ca49ca8dea226527a43ee6a32b
Oleh <krupao980@gmail.com>
2023-11-27 14:14:08 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('send:daily-message')->daily();
+        $schedule->command('send:send-welcome')->daily();
     }
 
     /**
